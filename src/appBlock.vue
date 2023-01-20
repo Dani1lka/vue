273622ebbf960f6f9a916a3408980adcbf5c2bc1
dani1lka <ostapchuk.d.03@mail.ru>
2023-01-20 15:56:13 +0200

    all

---
+++ src/appBlock.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="card">
+        <slot name="header"></slot>
+        <slot name="default"></slot>
+        <slot name="footer"></slot>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'appBlock'
+}
+</script>
