af0d683d3a4f65cc927778e4f614d6805c50f081
dani1lka <ostapchuk.d.03@mail.ru>
2023-01-20 15:43:43 +0200

    change all file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="container pt-1">
+  <div class="card">
+    <h1>Динамический компанент</h1>
+    <appButton color="">One</appButton>
+    <appButton color="primary" @acrive="active='two'">Two</appButton>
+  </div>
+  <!-- <div>
+    <app-list>
+      <template #default="{idx,iter}">
+        <span style="color:red" >
+          <strong>{{idx+1}}</strong>
+          Item: {{ iter }}
+        </span>
+      </template>
+    </app-list>
+  </div> -->
+
+
+  <!-- <appBlock>
+    <template v-slot:header>
+      <h3>Это заголовок карточки</h3>
+    </template>
+    
+    <template v-slot:default>
+      <hr>
+      <h1>Default text</h1>
+      <hr>
+    </template>
+  
+  <template v-slot:footer>
+    <h2>Это footer</h2>
+  </template>
+  </appBlock> -->
+
+
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import appBlock from './appBlock.vue';
+// import AppList from './appList.vue';
 
+import appTextOne from './appTextOne.vue'
+import appTextTwo from './appTextTwo.vue'
+import appButton from './appButton.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{appTextOne,appTextTwo,appButton},
+  data(){
+    return{
+      active:'one',
+    }
+  },
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
